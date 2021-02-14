7c4fbfdb5ecf2281061c49c0d9f42f6867e3e4ec
Sambhrama754 <sambhramakhushi@hotmail.com>
2021-02-14 10:42:20 +0530

    msg

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,60 @@
 <template>
-  
+  <div class="body">
+    <div id="container">
+      <div id="contacts">
+        <div id="contacts--contact">
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+      <div id="mentions">
+        <p></p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Open+Sans");
 
+.body {
+  display: flex;
+  background-color: #f0f8ff;
+  justify-content: center;
+  align-items: flex-end;
+  font-family: Open Sans;
+}
+
+#container {
+  padding-top: 20px;
+  margin-top: 20px;
+  width: 100vw;
+  color: white;
+  background-color: #03020c;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#contacts {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+}
+#contacts--contact {
+  text-align: right;
+}
+#contacts--social div {
+  height: 30px;
+  width: 30px;
+  background-color: white;
+  border-radius: 50%;
+}
+#mentions p {
+  font-size: 0.5em;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,144 +1,137 @@
 <template>
   <div>
-  <div class="pimg1">
-    <div class="ptext">
-      <span class="border">
-        Parallax Website
-      </span>
-    </div>
-  </div>
-
-  <section class="section section-light">
-    <h2>Section One</h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugit amet tempora facere dolores nisi facilis consequatur, odio hic minima nostrum. Perferendis eos earum praesentium, blanditiis sapiente labore aliquam ipsa architecto vitae. Minima soluta temporibus voluptates inventore commodi cumque esse suscipit optio aliquam et, dolorem a cupiditate nihil fuga laboriosam fugiat placeat dignissimos! Unde eveniet placeat quisquam blanditiis voluptatem doloremque fugiat dolor repellendus ratione in. Distinctio provident dolorem modi cumque illo enim quidem tempora deserunt nostrum voluptate labore repellat quisquam quasi cum suscipit dolore ab consequuntur, ad porro earum temporibus. Laborum ad temporibus ex, omnis!
-    </p>
-  </section>
-
-  <div class="pimg2">
-    <div class="ptext">
-      <span class="border trans">
-        Image Two Text
-      </span>
-    </div>
-  </div>
-
   <section class="section section-dark">
-    <h2>Section Two</h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugit amet tempora facere dolores nisi facilis consequatur, odio hic minima nostrum. Perferendis eos earum praesentium, blanditiis sapiente labore aliquam ipsa architecto vitae. Minima soluta temporibus voluptates inventore commodi cumque esse suscipit optio aliquam et, dolorem a cupiditate nihil fuga laboriosam fugiat placeat dignissimos! Unde eveniet placeat quisquam blanditiis voluptatem doloremque fugiat dolor repellendus ratione in.
-    </p>
-  </section>
+      <h2>Section One</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt,
+        laudantium, quibusdam? Nobis, delectus, commodi, fugit amet tempora
+        facere dolores nisi facilis consequatur, odio hic minima nostrum.
+        Perferendis eos earum praesentium, blanditiis sapiente labore aliquam
+        ipsa architecto vitae. Minima soluta temporibus voluptates inventore
+        commodi cumque esse suscipit optio aliquam et, dolorem a cupiditate
+        nihil fuga laboriosam fugiat placeat dignissimos! Unde eveniet placeat
+        quisquam blanditiis voluptatem doloremque fugiat dolor repellendus
+        ratione in. Distinctio provident dolorem modi cumque illo enim quidem
+        tempora deserunt nostrum voluptate labore repellat quisquam quasi cum
+        suscipit dolore ab consequuntur, ad porro earum temporibus. Laborum ad
+        temporibus ex, omnis!
+      </p>
+    </section>
 
-  <div class="pimg3">
-    <div class="ptext">
-      <span class="border trans">
-        Image Three Text
-      </span>
+    <div class="pimg2">
+      <div class="ptext">
+        <span class="border trans"> Image Two Text </span>
+      </div>
     </div>
-  </div>
 
-  <section class="section section-dark">
-    <h2>Section Three</h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugit amet tempora facere dolores nisi facilis consequatur, odio hic minima nostrum. Perferendis eos earum praesentium, blanditiis sapiente labore aliquam ipsa architecto vitae. Minima soluta temporibus voluptates inventore commodi cumque esse suscipit optio aliquam et, dolorem a cupiditate nihil fuga laboriosam fugiat placeat dignissimos! Unde eveniet placeat quisquam blanditiis voluptatem doloremque fugiat dolor repellendus ratione in.
-    </p>
-  </section>
+    <section class="section section-dark">
+      <h2>Section Two</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt,
+        laudantium, quibusdam? Nobis, delectus, commodi, fugit amet tempora
+        facere dolores nisi facilis consequatur, odio hic minima nostrum.
+        Perferendis eos earum praesentium, blanditiis sapiente labore aliquam
+        ipsa architecto vitae. Minima soluta temporibus voluptates inventore
+        commodi cumque esse suscipit optio aliquam et, dolorem a cupiditate
+        nihil fuga laboriosam fugiat placeat dignissimos! Unde eveniet placeat
+        quisquam blanditiis voluptatem doloremque fugiat dolor repellendus
+        ratione in.
+      </p>
+    </section>
 
-  <div class="pimg1">
-    <div class="ptext">
-      <span class="border">
-        Traversy Media
-      </span>
+    <div class="pimg3">
+      <div class="ptext">
+        <span class="border trans"> Image Three Text </span>
+      </div>
     </div>
-  </div>
+
+    <section class="section section-dark">
+      <h2>Section Three</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt,
+        laudantium, quibusdam? Nobis, delectus, commodi, fugit amet tempora
+        facere dolores nisi facilis consequatur, odio hic minima nostrum.
+        Perferendis eos earum praesentium, blanditiis sapiente labore aliquam
+        ipsa architecto vitae. Minima soluta temporibus voluptates inventore
+        commodi cumque esse suscipit optio aliquam et, dolorem a cupiditate
+        nihil fuga laboriosam fugiat placeat dignissimos! Unde eveniet placeat
+        quisquam blanditiis voluptatem doloremque fugiat dolor repellendus
+        ratione in.
+      </p>
+    </section>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 <style>
-body, html{
-  height:100%;
-  margin:0;
-  font-size:16px;
-  font-family:"Lato", sans-serif;
-  font-weight:400;
-  line-height:1.8em;
-  color:#666;
+.pimg1,
+.pimg2,
+.pimg3 {
+  position: relative;
+  opacity: 0.7;
+  background-position: center;
+  background-size: cover;
+  background-attachment: fixed;
 }
 
-.pimg1, .pimg2, .pimg3{
-  position:relative;
-  opacity:0.70;
-  background-position:center;
-  background-size:cover;
-  background-repeat:no-repeat;
-
-  /*
-    fixed = parallax
-    scroll = normal
-  */
-  background-attachment:fixed;
+.pimg1 {
+  background-image: url("../static/image1.jpg");
+  min-height: 100%;
 }
 
-.pimg1{
-  background-image:url('../static/image1.jpg');
-  min-height:100%;
+.pimg2 {
+  background-image: url("../static/image2.jpg");
+  min-height: 400px;
 }
 
-.pimg2{
-  background-image:url('../static/image2.jpg');
-  min-height:400px;
+.pimg3 {
+  background-image: url("../static/image3.jpg");
+  min-height: 400px;
 }
 
-.pimg3{
-  background-image:url('../static/image3.jpg');
-  min-height:400px;
+.section {
+  text-align: center;
+  padding: 50px 80px;
 }
 
-.section{
-  text-align:center;
-  padding:50px 80px;
+.section-light {
+  background-color: #f4f4f4;
+  color: #666;
 }
 
-.section-light{
-  background-color:#f4f4f4;
-  color:#666;
+.section-dark {
+  background-color: #282e34;
+  color: #ddd;
 }
 
-.section-dark{
-  background-color:#282e34;
-  color:#ddd;
+.ptext {
+  position: absolute;
+  top: 50%;
+  width: 100%;
+  text-align: center;
+  color: #000;
+  font-size: 27px;
+  letter-spacing: 8px;
+  text-transform: uppercase;
 }
 
-.ptext{
-  position:absolute;
-  top:50%;
-  width:100%;
-  text-align:center;
-  color:#000;
-  font-size:27px;
-  letter-spacing:8px;
-  text-transform:uppercase;
+.ptext .border {
+  background-color: #111;
+  color: #fff;
+  padding: 20px;
 }
 
-.ptext .border{
-  background-color:#111;
-  color:#fff;
-  padding:20px;
+.ptext .border.trans {
+  background-color: transparent;
 }
 
-.ptext .border.trans{
-  background-color:transparent;
-}
-
-@media(max-width:568px){
-  .pimg1, .pimg2, .pimg3{
-    background-attachment:scroll;
+@media (max-width: 568px) {
+  .pimg1,
+  .pimg2,
+  .pimg3 {
+    background-attachment: scroll;
   }
 }
 </style>
